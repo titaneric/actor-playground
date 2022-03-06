e1967b5d991a51d39dfed5b8e6cda118436641c6
titaneric <chenyihuang001@gmail.com>
2022-03-06 21:01:13 +0800

    Read byte stream from run request

--- src/run.rs
+++ src/run.rs
@@ -1,13 +1,21 @@
 use actix_web::{post, web, HttpResponse};
 use log::info;
 use serde::Deserialize;
+use futures::StreamExt;
 
 #[derive(Deserialize)]
 struct RunReq {
     built_binary: Vec<u8>,
 }
 #[post("/run")]
-async fn run_handler(run_req: web::Json<RunReq>) -> HttpResponse {
-    info!("Welcome {:?}!", run_req.built_binary);
+async fn run_handler(mut body: web::Payload) -> HttpResponse {
+    let mut bytes = web::BytesMut::new();
+    while let Some(item) = body.next().await {
+        let item = item.unwrap();
+        bytes.extend_from_slice(&item);
+    }
+    let run_req: RunReq= serde_json::from_slice(&bytes).unwrap();
+    println!("Chunk: {:?}",run_req.built_binary.len() );
+
     HttpResponse::Ok().json("ok")
 }
